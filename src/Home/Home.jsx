14183bfcdbba6c503d1e20eb6df31d272156c9de
Will Mitchell <post4wjm@gmail.com>
2020-02-05 16:54:09 +1100

    Final bug fixes for nav menu

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -7,20 +7,18 @@ const Home = ({ card }) => {
   const id = `card${card}`;
   return (
     <div id={id} className="home">
-      <div className="home-image-low-res">
-        <div className="home-image-high-res">
-          <div className="home-container">
-            <h2 className="home-heading-2">HI, I'M WILL</h2>
-            <p className="home-description">
-              Junior Web, Mobile & Application Developer
-            </p>
-            <ScrollTo selector={`#card${"about"}`}>
-              <svg className="expandarrow-icon">
-                <use xlinkHref={`${icons}#icon-expand-arrow`} />
-              </svg>
-            </ScrollTo>
-          {/* </div> */}
-        {/* </div> */}
+      <div className="home-image-low-res" />
+      <div className="home-image-high-res" />
+      <div className="home-container">
+        <h2 className="home-heading-2">HI, I'M WILL</h2>
+        <p className="home-description">
+          Junior Web, Mobile & Application Developer
+        </p>
+        <ScrollTo selector={`#card${"about"}`}>
+          <svg className="expandarrow-icon">
+            <use xlinkHref={`${icons}#icon-expand-arrow`} />
+          </svg>
+        </ScrollTo>
       </div>
     </div>
   );
