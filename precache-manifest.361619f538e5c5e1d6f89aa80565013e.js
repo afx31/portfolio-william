8956ba95bd264df6723b019f538af41f7dddede6
Will Mitchell <post4wjm@gmail.com>
2020-02-19 17:29:29 +1100

    Updates

---
+++ precache-manifest.361619f538e5c5e1d6f89aa80565013e.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eee37cbda249cd753df0301f45aca97a",
+    "url": "/portfolio-william/index.html"
+  },
+  {
+    "revision": "8720ee809304767a7e8c",
+    "url": "/portfolio-william/static/css/main.ae013824.chunk.css"
+  },
+  {
+    "revision": "33abd4a58a97f29e6639",
+    "url": "/portfolio-william/static/js/2.8f0417d8.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/portfolio-william/static/js/2.8f0417d8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8720ee809304767a7e8c",
+    "url": "/portfolio-william/static/js/main.02bec2b8.chunk.js"
+  },
+  {
+    "revision": "e015bdfd745c1812a49e",
+    "url": "/portfolio-william/static/js/runtime-main.5a6ee2aa.js"
+  },
+  {
+    "revision": "2af7fc9117fe5da76dc2132ba1226b7d",
+    "url": "/portfolio-william/static/media/gridtemplateImage.2af7fc91.png"
+  },
+  {
+    "revision": "30e409133778d2d75d951f6469fe5f09",
+    "url": "/portfolio-william/static/media/portfolioImage.30e40913.png"
+  },
+  {
+    "revision": "43476b991eaef8ec83851141886ab173",
+    "url": "/portfolio-william/static/media/sprite.43476b99.svg"
+  },
+  {
+    "revision": "28a143416ff7bd0160d54c48d74cfc19",
+    "url": "/portfolio-william/static/media/wallpaper-high-res.28a14341.jpg"
+  },
+  {
+    "revision": "709215f33260a514714429f9a5d9eb12",
+    "url": "/portfolio-william/static/media/wallpaper-low-res.709215f3.png"
+  },
+  {
+    "revision": "a963da404cf6f2246699ac72cea9c065",
+    "url": "/portfolio-william/static/media/wallpaper-superlow-res.a963da40.png"
+  },
+  {
+    "revision": "ccd334c85e645a720a042480f831b829",
+    "url": "/portfolio-william/static/media/wireframes.ccd334c8.jpg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/portfolio-william/precache-manifest.361619f538e5c5e1d6f89aa80565013e.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio-william/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
