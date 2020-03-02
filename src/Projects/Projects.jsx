662c378ec43b31500c00411091ce81e61c7b344e
Will Mitchell <post4wjm@gmail.com>
2020-03-02 22:31:33 +1100

    adding new project

--- src/Projects/Projects.jsx
+++ src/Projects/Projects.jsx
@@ -3,16 +3,26 @@ import "./Projects.css";
 import ProjectCard from "./ProjectCard/ProjectCard";
 import gridtemplateImage from "../img/gridtemplateImage.png";
 import portfolioImage from "../img/portfolioImage.png";
+import mernImage from "../img/mernImage.png";
 
 const Projects = () => {
   const projects = [
+    {
+      title: "MERN-Stack Template",
+      image: mernImage,
+      demoLink: "https://rocky-tundra-15421.herokuapp.com/",
+      githubLink: "https://github.com/afx31/mern-template",
+      description:
+        "This is a MERN-Stack template website containing a user account system where the user can register & login through their own account. It was created to show my API routing/MERN skillset, to develop it more & use for other projects as a starting template.",
+      languages: ["MongoDB", "Express.js", "React", "Node.js"]
+    },
     {
       title: "Grid Template Example",
       image: gridtemplateImage,
       demoLink: "https://afx31.github.io/information-grid-website/",
       githubLink: "https://github.com/afx31/information-grid-website",
       description:
-        "This is an example website for my portfolio to show a simple grid layout page with information and images, which could easily be adapted into a business landing page or a shop cartel website.",
+        "This is a example website for my portfolio to show a simple grid layout page with information and images, which could easily be adapted into a business landing page or a shop cartel website.",
       languages: ["HTML", "CSS", "JavaScript"]
     },
     {
